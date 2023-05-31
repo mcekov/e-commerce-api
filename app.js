23de60345b73fc8377b40b4b504359f23a9765bd
Momchil Tsekov <mtsekov@zohomail.eu>
2023-05-31 23:04:38 +0300

    morgan setup

--- app.js
+++ app.js
@@ -2,13 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const connectDB = require("./db/connect");
 require("dotenv").config();
 require("express-async-errors");
 
+/* CONNECT TO DB */
+const connectDB = require("./db/connect");
+
+const morgan = require("morgan");
+
+/* MIDDLEWARE */
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.use(morgan("tiny"));
 app.use(express.json());
 
 app.get("/", (req, res) => {
