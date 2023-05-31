fc16dda787ea9ca49db3eab698724eb8e708e6e6
Momchil Tsekov <mtsekov@zohomail.eu>
2023-05-31 22:19:18 +0300

    Connection to Mongo Atlas

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('04 Store API')
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port: ${PORT}`);
+      console.log("DB Connected...");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
