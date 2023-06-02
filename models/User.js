f5d4ea34d1331e2256c10ebb96bc336af234f073
Momchil Tsekov <mtsekov@zohomail.eu>
2023-06-02 22:48:48 +0300

    Register route

--- models/User.js
+++ models/User.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 
-const UserSchema = new mongoose.UserSchema({
+mongoose.set("strictQuery", true);
+
+const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, "Please provide name"],
