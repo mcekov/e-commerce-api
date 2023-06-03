6f984abfcc30be637d376ae0b2f9d40002ea528f
Momchil Tsekov <mtsekov@zohomail.eu>
2023-06-03 09:25:20 +0300

    user roles

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,18 @@ const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 
 const register = async (req, res) => {
-  const user = await User.create(req.body);
+  const { email, name, password } = req.body;
+
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("Email already exists");
+  }
+
+  /* First reg. user is ADMIN */
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
+
+  const user = await User.create({ name, email, password, role });
   res.status(StatusCodes.CREATED).json({ user });
 };
 
--- models/User.js
+++ models/User.js
@@ -24,7 +24,7 @@ const UserSchema = new mongoose.Schema({
     required: [true, "Please provide password"],
     minLength: 6,
   },
-  roles: {
+  role: {
     type: String,
     enum: ["admin", "user"],
     default: "user",
