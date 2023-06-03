ddf1106cc963664657cefcf291deca6ebf4dd520
Momchil Tsekov <mtsekov@zohomail.eu>
2023-06-03 11:31:36 +0300

    ATTACH TOKEN TO COOKIE

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,7 +20,14 @@ const register = async (req, res) => {
   const tokenUser = { name: user.name, userId: user.id, role: user.role };
   const token = createJWT({ payload: tokenUser });
 
-  res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
+  /* ATTACH TOKEN TO COOKIE */
+  const oneDay = 1000 * 60 * 60 * 24;
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+  });
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
 
 const login = async (req, res) => {
