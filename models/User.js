0c77072f84e8de58ad2d90f6e95aa4d2fef908d4
Momchil Tsekov <mtsekov@zohomail.eu>
2023-06-03 09:43:55 +0300

    password hash with bcryptjs

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 mongoose.set("strictQuery", true);
 
@@ -30,5 +31,16 @@ const UserSchema = new mongoose.Schema({
     default: "user",
   },
 });
+
+/* Before save document we hash the password */
+UserSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
 
 module.exports = mongoose.model("User", UserSchema);
