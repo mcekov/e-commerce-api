57417e257910a4b27e9e5e5ceea8c56fe0fb0c75
m <mtsekov@zohomail.eu>
2023-06-26 09:56:19 +0300

    authController and orderController messages update

--- constants/messages.js
+++ constants/messages.js
@@ -1,1 +1,13 @@
-export const MESSAGES = {};
+const MESSAGES = {
+  /* CONTROLLERS */
+  emailExist: 'Email already exists',
+  emptyEmailOrPassword: 'Please provide email and password',
+  invalidCredentials: 'Invalid credentials',
+  logoutUserMessage: 'User logged out',
+
+  invalidOrder: 'No order with id',
+  emptyCart: 'No cart items provided',
+  emtyTaxOrShipping: 'Please provide tax and shipping fee',
+};
+
+module.exports = { MESSAGES };
--- controllers/authController.js
+++ controllers/authController.js
@@ -2,13 +2,14 @@ const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
 const { attachCookiesToResponse, createTokenUser } = require('../utils');
+const { MESSAGES } = require('../constants/messages');
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
 
   const emailAlreadyExists = await User.findOne({ email });
   if (emailAlreadyExists) {
-    throw new CustomError.BadRequestError('Email already exists');
+    throw new CustomError.BadRequestError(MESSAGES.emailExist);
   }
 
   /* First reg. user is ADMIN */
@@ -25,17 +26,17 @@ const login = async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    throw new CustomError.BadRequestError('Please provide email and password');
+    throw new CustomError.BadRequestError(MESSAGES.emptyEmailOrPassword);
   }
 
   const user = await User.findOne({ email });
   if (!user) {
-    throw new CustomError.BadRequestError('Invalid credentials');
+    throw new CustomError.BadRequestError(MESSAGES.invalidCredentials);
   }
 
   const isPasswordCorrect = await user.comparePassword(password);
   if (!isPasswordCorrect) {
-    throw new CustomError.BadRequestError('Invalid credentials');
+    throw new CustomError.BadRequestError(MESSAGES.invalidCredentials);
   }
 
   const tokenUser = createTokenUser(user);
@@ -46,7 +47,7 @@ const login = async (req, res) => {
 const logout = async (req, res) => {
   res.cookie('token', 'logout', { httpOnly: true, expires: new Date(Date.now()) });
 
-  res.status(StatusCodes.OK).json({ message: 'User logged out' });
+  res.status(StatusCodes.OK).json({ message: MESSAGES.logoutUserMessage });
 };
 
 module.exports = { register, login, logout };
